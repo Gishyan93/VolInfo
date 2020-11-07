3352fa24006bd7400df4c5beb727536a46e7b2f7
Tigran Gishyan <tigran.gishian@gmail.com>
2020-11-08 01:02:35 +0400

    Was added.

---
+++ VolInfo/Scene1/Main/Controller/MainNavigationController.swift
@@ -0,0 +1,19 @@
+//
+//  MainNavigationController.swift
+//  Volinfo
+//
+//  Created by Tigran Gishyan on 10/25/20.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let homeController = CustomTabBarController()
+        viewControllers = [homeController]
+    }
+    
+   
+}
