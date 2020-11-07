0fadeebc378e6fa6b3e42a7eda8cf2dc7ad4be7b
Tigran Gishyan <tigran.gishian@gmail.com>
2020-11-08 02:52:53 +0400

    Project setup

--- VolInfo/Supporting Files/Inherited/BaseCell.swift
+++ VolInfo/Supporting Files/Inherited/BaseCell.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 
-//Cause we are a good programmers, we are creating the base class not to dublicate the code
 class BaseCell : UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         setupViews()
-        
     }
     
     func setupViews() {
@@ -23,7 +20,5 @@ class BaseCell : UICollectionViewCell {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
 }
 
